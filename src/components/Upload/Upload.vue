947b65e2f2e209d6612c09c6d98e207d2b62d9ba
SidKwok <oceankwok@hotmail.com>
2016-09-12 10:50:29 +0800

    Upload: finish props

--- src/components/Upload/Upload.vue
+++ src/components/Upload/Upload.vue
@@ -1,21 +1,72 @@
 <template lang="html">
-    <div class="Upload">
-
+    <div class="uploadCover">
+        <label v-el:upload
+            class="upload"
+            @dragenter.prevent.stop="dragenter"
+            @dragleave.prevent.stop="dragleave"
+            @dragover.prevent.stop
+            @drop.prevent.stop="save($event.dataTransfer.files[0])"
+            :style="{
+                backgroundImage: `url(${imgUrl})`,
+                width: `${width}px`,
+                height: `${height}px`
+                }">
+            <input
+                type="file"
+                accept="image/*"
+                class="dn"
+                @change="save($event.target.files[0])"
+            >
+        </label>
+        <p>
+            {{ info }}
+        </p>
     </div>
 </template>
 
 <script>
     export default {
-        // two kind of file
-        // actual file and image
-        type: {
-            type: String,
-            default: 'file'
+        props: {
+            // two kind of file
+            // actual file and image
+            fileType: {
+                type: String,
+                default: 'file'
+            },
+            // default image
+            // should be a url
+            img: String,
+            // send the formdata out,
+            // so you can upload the image
+            formData: {
+                required: true,
+                twoWay: true
+            },
+            // limit of the image
+            // KB
+            limit: {
+                type: Number,
+                default: 40
+            },
         },
-        // limits of the file
-        limits: {
-            type: Number,
-            default: 40000
+        methods: {
+            dragenter() {
+                this.$els.file.classList.add('red-line');
+            },
+            dragleave() {
+                this.$els.file.classList.remove('red-line');
+            },
+            saveImage(file) {
+                this.$els.file.classList.remove('red-line');
+                if (file.size <= (this.limit * 1000)) {
+                    this.formData = file;
+                    let img = new FileReader();
+                    img.readAsDataURL(file);
+                    img.onload = e => this.imgUrl = e.currentTarget.result;
+                } else {
+                    this.$emit('error')
+                }
+            }
         }
     };
 </script>
